da4d47dda9f4d083f7491eca3539127fe74219d1
Denis Memiev <denismemiev@gmail.com>
2021-05-25 16:39:11 +0300

    updating the app with a shop page, collection preview component and collection item componen

--- coronet-clothing/src/components/collection-preview/collection-preview-component.jsx
+++ coronet-clothing/src/components/collection-preview/collection-preview-component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import CollectionItem from '../collection-item/collection-item.component'
 import './collection-preview-style.scss';
 
 
@@ -11,10 +11,8 @@ const CollectionPreview = ({ title, items }) => (
         <div className='preview'>
             {items
                 .filter((item, idx) => idx < 4)
-                .map((item) => (
-                    <div key={item.id}>
-                        {item.name}
-                    </div>
+                .map(({ id, ...otherItemProps }) => (
+                    <CollectionItem key={id} {...otherItemProps} />
                 ))
             }
             {/* {items.map((item, idx) => {
