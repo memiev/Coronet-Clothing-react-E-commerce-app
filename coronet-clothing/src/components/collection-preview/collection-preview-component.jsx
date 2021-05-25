7d20b94770991d36b380afaa07991d6425a00797
Denis Memiev <denismemiev@gmail.com>
2021-05-25 14:03:21 +0300

    feature collection preview and data array

---
+++ coronet-clothing/src/components/collection-preview/collection-preview-component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './collection-preview-style.scss';
+
+
+const CollectionPreview = ({ title, items }) => (
+    <div className='collection-preview'>
+        <h1 className='title'>
+            {title.toUpperCase()}
+        </h1>
+        <div className='preview'>
+            {items
+                .filter((item, idx) => idx < 4)
+                .map((item) => (
+                    <div key={item.id}>
+                        {item.name}
+                    </div>
+                ))
+            }
+            {/* {items.map((item, idx) => {
+                return idx < 4 && <div key={item.id}>{item.name}</div>;
+            })} */}
+        </div>
+    </div>
+)
+
+export default CollectionPreview;
+
+
+
