7e91f1e68d005527d30fbbae0c24d774e75e3dd3
Denis Memiev <denismemiev@gmail.com>
2021-06-05 17:47:51 +0300

    implemented firebase utils, included ability to store authenticated users into firestore database

--- coronet-clothing/src/App.js
+++ coronet-clothing/src/App.js
@@ -4,21 +4,39 @@ import HomePage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
 import "./App.css";
 import Header from "./components/header/header.component";
-import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-and-up.component";
-import { auth } from "./firebase/firebase.utils";
+import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-and-sign-up.component";
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 
 class App extends React.Component {
   constructor() {
     super();
+
     this.state = {
       currentUser: null,
     };
   }
+
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
@@ -29,7 +47,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={Shop} />
